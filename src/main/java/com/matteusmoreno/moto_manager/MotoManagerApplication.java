3c74c0246950f9caaec28f2e734359f2d6ebf68b
Matteus Moreno <matteus_moreno@live.com>
2024-08-06 09:14:15 -0300

    initial commit

---
+++ src/main/java/com/matteusmoreno/moto_manager/MotoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matteusmoreno.moto_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matteusmoreno/moto_manager/MotoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matteusmoreno.moto_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
