cf0aa563b7aca24d22db7f6419e5d7af26befb2b
Matteus Moreno <matteus_moreno@live.com>
2024-09-05 14:39:06 -0300

    adicionando teste à classe ProductDetailsResponse

---
+++ src/test/java/com/matteusmoreno/moto_manager/product/response/ProductDetailsResponseTest.java
@@ -0,0 +1,39 @@
+package com.matteusmoreno.moto_manager.product.response;
+
+import com.matteusmoreno.moto_manager.product.entity.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Product Details Response Tests")
+class ProductDetailsResponseTest {
+
+    @Test
+    @DisplayName("Should return a Product Details Response successfully")
+    void shouldReturnProductDetailsResponseSuccessfully() {
+        Product product = new Product(1L, "PRODUCT NAME", "PRODUCT DESCRIPTION", "MANUFACTURER", new BigDecimal("100.00"), 10, LocalDateTime.now(), null, null, true);
+
+        ProductDetailsResponse result = new ProductDetailsResponse(product);
+
+        assertAll(
+            () -> assertEquals(product.getId(), result.id()),
+            () -> assertEquals(product.getName(), result.name()),
+            () -> assertEquals(product.getDescription(), result.description()),
+            () -> assertEquals(product.getManufacturer(), result.manufacturer()),
+            () -> assertEquals(product.getPrice(), result.price()),
+            () -> assertEquals(product.getQuantity(), result.quantity()),
+            () -> assertEquals(product.getCreatedAt(), result.createdAt()),
+            () -> assertEquals(product.getUpdatedAt(), result.updatedAt()),
+            () -> assertEquals(product.getDeletedAt(), result.deletedAt()),
+            () -> assertEquals(product.getActive(), result.active())
+        );
+    }
+
+}
