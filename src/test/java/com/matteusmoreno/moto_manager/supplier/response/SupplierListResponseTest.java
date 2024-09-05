35b783b485bbae90bb751bc2d3ce1eb6bfef4357
Matteus Moreno <matteus_moreno@live.com>
2024-09-05 11:13:49 -0300

    adicionando teste à classe SupplierListResponse

---
+++ src/test/java/com/matteusmoreno/moto_manager/supplier/response/SupplierListResponseTest.java
@@ -0,0 +1,32 @@
+package com.matteusmoreno.moto_manager.supplier.response;
+
+import com.matteusmoreno.moto_manager.supplier.entity.Supplier;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Supplier List Response Tests")
+class SupplierListResponseTest {
+
+    @Test
+    @DisplayName("Should return a Supplier List Response successfully")
+    void shouldReturnSupplierListResponseSuccessfully() {
+        Supplier supplier = new Supplier(1L, "SUPPLIER NAME", "00.000.000/0000-00", "(00)000000000", "supplier@email.com", LocalDateTime.now(), null, null, true);
+
+        SupplierListResponse result = new SupplierListResponse(supplier);
+
+        assertAll(
+            () -> assertEquals(supplier.getId(), result.id()),
+            () -> assertEquals(supplier.getName(), result.name()),
+            () -> assertEquals(supplier.getCnpj(), result.cnpj()),
+            () -> assertEquals(supplier.getPhone(), result.phone()),
+            () -> assertEquals(supplier.getEmail(), result.email())
+        );
+    }
+}
