ea3863f4c100963d7d6b36158baa3a3280d821f5
Matteus Moreno <matteus_moreno@live.com>
2024-09-14 10:21:30 -0300

    alterando configurações CORS

--- src/main/java/com/matteusmoreno/moto_manager/config/WebConfiguration.java
+++ src/main/java/com/matteusmoreno/moto_manager/config/WebConfiguration.java
@@ -13,9 +13,10 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:8000",
                         "http://localhost:8080",
-                        "https://mango-moss-08608420f.5.azurestaticapps.net",
-                        "https://moto-manager-frontend.vercel.app")
-                .allowedMethods("*")
+                        "https://mango-moss-08608420f.5.azurestaticapps.net", // antiga URL
+                        "https://red-rock-0584fbc0f.5.azurestaticapps.net", // nova URL no Azure
+                        "https://moto-manager-frontend.vercel.app") // Vercel
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
